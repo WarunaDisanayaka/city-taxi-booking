d92bf99f78104007f34a17b3317c941af6fd2441
Waruna <86902580+WarunaDisanayaka@users.noreply.github.com>
2024-09-29 08:40:36 +0530

    Added dashboard

--- src/routes/Routes.jsx
+++ src/routes/Routes.jsx
@@ -9,7 +9,6 @@ import CarRental from '../pages/CarRental';
 import CarBooking from '../pages/CarBooking';
 import Products from '../pages/Products';
 import ProductDetails from '../pages/ProductDetails';
-
 import Checkout from '../pages/Checkout';
 import Blogs from '../pages/Blogs';
 import BlogDetails from '../pages/BlogDetails';
@@ -19,36 +18,214 @@ import Contact from '../pages/Contact';
 import NotFound from '../pages/NotFound';
 import CarBookingView from '../pages/CarBooking';
 import BookingDetails from '../pages/BookingDetails';
+import Header from '../components/Header'; // Import Header
+import Footer from '../components/Footer'; // Import Footer
+import Dashboard from '../components/Admin/dashboard';
 
-const routes = () => {
+const RoutesConfig = () => {
   return (
     <Routes>
-      <Route path="/" element={<Navigate to="/home" />} />
-      <Route path="/home" element={<Home />} />
+      <Route
+        path="/"
+        element={
+          <>
+            <Header />
+            <Navigate to="/home" />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/home"
+        element={
+          <>
+            <Header />
+            <Home />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/about"
+        element={
+          <>
+            <Header />
+            <About />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/services"
+        element={
+          <>
+            <Header />
+            <Services />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/service-details/:id"
+        element={
+          <>
+            <Header />
+            <ServiceDetails />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/car-listing"
+        element={
+          <>
+            <Header />
+            <CarRental />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/car-details/:id"
+        element={
+          <>
+            <Header />
+            <CarBooking />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/booking/:id"
+        element={
+          <>
+            <Header />
+            <CarBookingView />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/products"
+        element={
+          <>
+            <Header />
+            <Products />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/product-details/:id"
+        element={
+          <>
+            <Header />
+            <ProductDetails />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/booking-details"
+        element={
+          <>
+            <Header />
+            <BookingDetails />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/checkout"
+        element={
+          <>
+            <Header />
+            <Checkout />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/blogs"
+        element={
+          <>
+            <Header />
+            <Blogs />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/blog-details/:id"
+        element={
+          <>
+            <Header />
+            <BlogDetails />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/signin"
+        element={
+          <>
+            <Header />
+            <SignIn />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/signup"
+        element={
+          <>
+            <Header />
+            <SignUp />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/contact"
+        element={
+          <>
+            <Header />
+            <Contact />
+            <Footer />
+          </>
+        }
+      />
+      <Route
+        path="/admin"
+        element={
+          <>
+            <Dashboard />
+          </>
+        }
+      />
+      <Route
+        path="*"
+        element={
+          <>
+            <Header />
+            <NotFound />
+            <Footer />
+          </>
+        }
+      />
 
-      <Route path="/about" element={<About />} />
-      <Route path="/services" element={<Services />} />
-      <Route path="/service-details/:id" element={<ServiceDetails />} />
-      <Route path="/car-listing" element={<CarRental />} />
-      <Route path="/car-details/:id" element={<CarBooking />} />
-
-      <Route path="/booking/:id" element={<CarBookingView />} />
-      
-      <Route path="/products" element={<Products />} />
-      <Route path="/product-details/:id" element={<ProductDetails />} />
-
-      <Route path="/booking-details" element={<BookingDetails />} />
-
-
-      <Route path="/checkout" element={<Checkout />} />
-      <Route path="/blogs" element={<Blogs />} />
-      <Route path="/blog-details/:id" element={<BlogDetails />} />
-      <Route path="/signin" element={<SignIn />} />
-      <Route path="/signup" element={<SignUp />} />
-      <Route path="/contact" element={<Contact />} />
-      <Route path="*" element={<NotFound />} />
+      <Route
+        path="*"
+        element={
+          <>
+            <Header />
+            <NotFound />
+            <Footer />
+          </>
+        }
+      />
     </Routes>
   );
 };
 
-export default routes;
+export default RoutesConfig;
